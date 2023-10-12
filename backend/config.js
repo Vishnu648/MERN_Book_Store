b0ad5a0dd7163a8a6d376e23420f3f1db7a039ce
VishnuVS <vishnusenthil222@gmail.com>
2023-10-12 20:05:34 +0530

    created a book

---
+++ backend/config.js
@@ -0,0 +1,7 @@
+const PORT=4000;
+const MONGODB_URL='mongodb+srv://vishnuvsj34:ZL1A2i3XljKlhOfT@mernbookstore.lbklz8f.mongodb.net/Books?retryWrites=true&w=majority'
+
+export {
+    PORT,
+    MONGODB_URL
+}
---
+++ backend/index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import { PORT, MONGODB_URL } from "./config.js";
+import mongoose from "mongoose";
+import Book from "./models/bookModel.js";
+
+const port = PORT || 3000;
+const app = express();
+
+app.use(express.json())
+
+app.get('/',(req,res) => {
+  res.send('home page')
+}
+)
+
+app.post("/books", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear) {
+      res.status(500).send({ message: "All Field is required" });
+    }
+
+    const newBook = {
+      title: req.body.title,
+      author: req.body.author,
+      publishYear: req.body.publishYear,
+    };
+
+    const book = await Book.create(newBook);
+
+    return res.status(201).send(book);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send(error.message);
+  }
+});
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Running on http://localhost:${port}`);
+    });
+    console.log("Connected to DB");
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
