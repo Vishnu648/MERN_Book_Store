d34ccd9eb525c1cbe67b60acea93e4f6c027fb06
VishnuVS <vishnusenthil222@gmail.com>
2023-10-13 09:54:06 +0530

    vite and tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
+
+  const [datas, setDatas] = useState([]);
+  
+  useEffect(() => {
+    
+  axios.get('http://localhost:4000/books')
+  .then(response=>setDatas(response.data.data))
+  }, [])
+  
+
   return (
-    <div>App</div>
+    <div>
+      {
+        datas.map((d) => {
+          return(
+            <h2 key={d._id}>{d.title}</h2>
+          )
+        }
+        )
+      }
+      APP
+    </div>
   )
 }
 
