8336f4944b10a9a8c88b77cca25d265c9eaa645f
VishnuVS <vishnusenthil222@gmail.com>
2023-10-13 09:25:21 +0530

    created frontend with vite

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { PORT, MONGODB_URL } from "./config.js";
 import mongoose from "mongoose";
+import cors from 'cors'
 
 import bookRoutes from "./routes/bookRoutes.js"
 
@@ -12,6 +13,12 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("home page");
 });
+
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods:['GET','POST','PUT','DELETE'],
+  allowedHeaders:['content-type']
+}))
 
 app.use('/books',bookRoutes)
 
